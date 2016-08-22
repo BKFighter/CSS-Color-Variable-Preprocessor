3aaa351a62d2cdb7754ae92d07a2780f893664c8
BKFighter <bkfergen@gmail.com>
2016-08-22 18:20:42 -0500

    Update CCV-Preprocessor.js

--- CCV-Preprocessor.js
+++ CCV-Preprocessor.js
@@ -1,9 +1,9 @@
 var ccvCSS;
 
-// For main color use "+main+" (including quotation marks)
-//For secondary color use "+secondary+" (including quotation marks)
+//For main color use "+main+" (including quotation marks). MAKE SURE TO USE ", not '.
+//For secondary color use "+secondary+" (including quotation marks). MAKE SURE TO USE ", not '.
 //DON'T surround the text in the file given with a ' or a "
-//DO
+//DO use ONLY the .txt format
 
 function process(url, main, secondary){
   
