bdf1f8bcd099509a90c99a1667abe55f845b8f00
BKFighter <bkfergen@gmail.com>
2016-08-22 18:05:23 -0500

    Update CCV-Preprocessor.js

--- CCV-Preprocessor.js
+++ CCV-Preprocessor.js
@@ -1,5 +1,10 @@
 var ccvCSS;
 
+// For main color use "+main+" (including quotation marks)
+//For secondary color use "+secondary+" (including quotation marks)
+//DON'T surround the text in the file given with a ' or a "
+//DO
+
 function process(url, main, secondary){
   
   mlhttp=new XMLHttpRequest();
